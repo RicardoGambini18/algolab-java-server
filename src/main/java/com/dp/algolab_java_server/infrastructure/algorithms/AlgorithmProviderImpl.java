5364bf3a3dc4ca0c40716b9ae2c5225bd44183f3
Ricardo Gambini <ricardo.gambini18@outlook.com>
2025-12-10 02:17:41 -0500

    feat: implement algorithm catalog endpoints

---
+++ src/main/java/com/dp/algolab_java_server/infrastructure/algorithms/AlgorithmProviderImpl.java
@@ -0,0 +1,132 @@
+package com.dp.algolab_java_server.infrastructure.algorithms;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.function.Function;
+import lombok.RequiredArgsConstructor;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.ObjectProvider;
+
+import com.dp.algolab_java_server.common.DesignPattern;
+import com.dp.algolab_java_server.domain.entities.Movie;
+import com.dp.algolab_java_server.domain.data_structures.Vector;
+import com.dp.algolab_java_server.domain.dtos.DataStructureDefinition;
+import com.dp.algolab_java_server.domain.dtos.SortAlgorithmDefinition;
+import com.dp.algolab_java_server.domain.algorithms.AlgorithmProvider;
+import com.dp.algolab_java_server.domain.data_structures.DataStructure;
+import com.dp.algolab_java_server.domain.dtos.SearchAlgorithmDefinition;
+import com.dp.algolab_java_server.domain.algorithms.sorting.SortAlgorithm;
+import com.dp.algolab_java_server.domain.algorithms.utils.AlgorithmProfiler;
+import com.dp.algolab_java_server.domain.exceptions.ResourceNotFoundException;
+import com.dp.algolab_java_server.domain.algorithms.searching.SearchAlgorithm;
+import com.dp.algolab_java_server.domain.algorithms.sorting.vector.BubbleSortVectorAlgorithm;
+import com.dp.algolab_java_server.domain.algorithms.searching.vector.LinearSearchVectorAlgorithm;
+
+@Component
+@RequiredArgsConstructor
+@DesignPattern(name = "Registry", solvedProblem = "Centraliza la configuración de algoritmos disponibles utilizando mapas internos. Esto permite asociar claves (strings) con sus constructores correspondientes de forma dinámica, eliminando la necesidad de lógica condicional compleja (if/else) para la instanciación.")
+public class AlgorithmProviderImpl implements AlgorithmProvider {
+  private final ObjectProvider<AlgorithmProfiler> algorithmProfilerFactory;
+  private final Map<String, DataStructureDefinition<SortAlgorithmDefinition>> sortingCatalogRegistry = new LinkedHashMap<>();
+  private final Map<String, DataStructureDefinition<SearchAlgorithmDefinition>> searchingCatalogRegistry = new LinkedHashMap<>();
+  private final Map<String, Map<String, Function<AlgorithmProfiler, SortAlgorithm<Movie, ?>>>> sortingAlgorithmsRegistry = new HashMap<>();
+  private final Map<String, Map<String, Function<AlgorithmProfiler, SearchAlgorithm<Movie, ?>>>> searchingAlgorithmsRegistry = new HashMap<>();
+
+  @PostConstruct
+  public void init() {
+    // Vector
+    Vector<?> vector = new Vector<>();
+    registerDataStructure(vector);
+    // Vector Sorting Algorithms
+    registerSortingAlgorithm(vector.getKey(),
+        algorithmProfiler -> new BubbleSortVectorAlgorithm<>(Movie::getId, algorithmProfiler));
+    // Vector Searching Algorithms
+    registerSearchingAlgorithm(vector.getKey(),
+        algorithmProfiler -> new LinearSearchVectorAlgorithm<>(Movie::getId, algorithmProfiler));
+  }
+
+  private void registerDataStructure(DataStructure<?> dataStructure) {
+    sortingCatalogRegistry.put(dataStructure.getKey(), dataStructure.toDefinition());
+    searchingCatalogRegistry.put(dataStructure.getKey(), dataStructure.toDefinition());
+  }
+
+  private void registerSortingAlgorithm(String dataStructureKey,
+      Function<AlgorithmProfiler, SortAlgorithm<Movie, ?>> sortAlgorithmConstructor) {
+    sortingAlgorithmsRegistry.computeIfAbsent(dataStructureKey, key -> new HashMap<>());
+
+    SortAlgorithm<Movie, ?> sortAlgorithm = sortAlgorithmConstructor.apply(null);
+    String sortAlgorithmKey = sortAlgorithm.getKey();
+
+    sortingAlgorithmsRegistry.get(dataStructureKey).put(sortAlgorithmKey, sortAlgorithmConstructor);
+
+    DataStructureDefinition<SortAlgorithmDefinition> dataStructureDefinition = sortingCatalogRegistry
+        .get(dataStructureKey);
+    if (dataStructureDefinition != null) {
+      dataStructureDefinition.getAlgorithms().add(sortAlgorithm.toDefinition());
+    }
+  }
+
+  private void registerSearchingAlgorithm(String dataStructureKey,
+      Function<AlgorithmProfiler, SearchAlgorithm<Movie, ?>> searchAlgorithmConstructor) {
+    searchingAlgorithmsRegistry.computeIfAbsent(dataStructureKey, key -> new HashMap<>());
+
+    SearchAlgorithm<Movie, ?> searchAlgorithm = searchAlgorithmConstructor.apply(null);
+    String searchAlgorithmKey = searchAlgorithm.getKey();
+
+    searchingAlgorithmsRegistry.get(dataStructureKey).put(searchAlgorithmKey, searchAlgorithmConstructor);
+
+    DataStructureDefinition<SearchAlgorithmDefinition> dataStructureDefinition = searchingCatalogRegistry
+        .get(dataStructureKey);
+    if (dataStructureDefinition != null) {
+      dataStructureDefinition.getAlgorithms().add(searchAlgorithm.toDefinition());
+    }
+  }
+
+  @Override
+  public List<DataStructureDefinition<SortAlgorithmDefinition>> getSortingAlgorithmsCatalog() {
+    return new ArrayList<>(sortingCatalogRegistry.values());
+  }
+
+  @Override
+  public List<DataStructureDefinition<SearchAlgorithmDefinition>> getSearchingAlgorithmsCatalog() {
+    return new ArrayList<>(searchingCatalogRegistry.values());
+  }
+
+  @Override
+  public SortAlgorithm<Movie, ?> getSortAlgorithm(String algorithmKey, String dataStructureKey) {
+    var dataStructureSortingAlgorithms = sortingAlgorithmsRegistry.get(dataStructureKey);
+    if (dataStructureSortingAlgorithms == null) {
+      throw new ResourceNotFoundException(
+          "La estructura de datos '" + dataStructureKey + "' no está soportada o no existe.");
+    }
+
+    var sortAlgorithmConstructor = dataStructureSortingAlgorithms.get(algorithmKey);
+    if (sortAlgorithmConstructor == null) {
+      throw new ResourceNotFoundException("El algoritmo de ordenamiento '" + algorithmKey
+          + "' no encontrado para la estructura '" + dataStructureKey + "'.");
+    }
+
+    return sortAlgorithmConstructor.apply(algorithmProfilerFactory.getObject());
+  }
+
+  @Override
+  public SearchAlgorithm<Movie, ?> getSearchAlgorithm(String algorithmKey, String dataStructureKey) {
+    var dataStructureSearchingAlgorithms = searchingAlgorithmsRegistry.get(dataStructureKey);
+    if (dataStructureSearchingAlgorithms == null) {
+      throw new ResourceNotFoundException(
+          "La estructura de datos '" + dataStructureKey + "' no está soportada o no existe.");
+    }
+
+    var searchAlgorithmConstructor = dataStructureSearchingAlgorithms.get(algorithmKey);
+    if (searchAlgorithmConstructor == null) {
+      throw new ResourceNotFoundException("El algoritmo de búsqueda '" + algorithmKey
+          + "' no encontrado para la estructura '" + dataStructureKey + "'.");
+    }
+
+    return searchAlgorithmConstructor.apply(algorithmProfilerFactory.getObject());
+  }
+}
